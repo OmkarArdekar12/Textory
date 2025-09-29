8264e1eb2b7d5c7519d65d033a24047be53d94fb
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-09-29 09:40:11 +0530

    Setup

---
+++ backend/src/lib/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  const MONGODB_URI = process.env.MONGODB_URI;
+  try {
+    const dbConnection = await mongoose.connect(MONGODB_URI);
+    console.log(`MongoDB Connected: ${dbConnection.connection.host}`);
+  } catch (err) {
+    console.log("MongoDB Connection Error: ", err);
+  }
+};
