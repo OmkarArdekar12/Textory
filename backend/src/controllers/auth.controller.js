8a3c6aab6d9eeed42170eaac7b3402195d80f4fd
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-09-29 23:41:19 +0530

    Controller Setup

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,11 @@
 export const signup = async (req, res) => {
   res.send("done");
 };
+
+export const login = async (req, res) => {
+  res.send("done");
+};
+
+export const logout = async (req, res) => {
+  res.send("done");
+};
