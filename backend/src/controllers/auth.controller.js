df87058e6e24223d3de80f35f0afe8192805f7b2
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-09-30 23:44:38 +0530

    updated

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -4,8 +4,18 @@ import bcrypt from "bcryptjs";
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
   try {
-    res.send("signup");
-  } catch (error) {}
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least of 6 characters" });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+  } catch (err) {}
 };
 
 export const login = (req, res) => {
