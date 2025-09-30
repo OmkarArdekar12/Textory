8e8b1abe994e1720cd63065e4373a1d32b22940b
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-10-01 01:25:14 +0530

    Updated

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,9 +1,14 @@
+import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
   try {
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
     if (password.length < 6) {
       return res
         .status(400)
@@ -26,11 +31,22 @@ export const signup = async (req, res) => {
     });
 
     if (newUser) {
-      //generate jwt token
+      generateToken(newUser._id, res); //generate jwt token
+      await newUser.save();
+
+      res.status(200).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePicture: newUser.profilePicture,
+      });
     } else {
       res.status(400).json({ message: "Invalid User Data" });
     }
-  } catch (err) {}
+  } catch (err) {
+    console.log("Error in Signup Controller: ", err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
 export const login = (req, res) => {
