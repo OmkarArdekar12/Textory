dc16c7d7c91629810d7fb3f26409bdea4abceb88
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-10-02 20:02:06 +0530

    Get Users for Sidebar

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,13 @@
+export const getUserForSidebar = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInUserId },
+    }).select("-password");
+
+    res.status(200).json(filteredUsers);
+  } catch (err) {
+    console.log("Error in GetUserForSidebar Controller: ", err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
