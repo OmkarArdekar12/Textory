29cf309c5e3ff9f988c9e0278b28920d35c96fb5
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-10-02 19:10:56 +0530

    CheckAuth Controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -112,3 +112,12 @@ export const updateProfile = async (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
+export const checkAuth = (req, res) => {
+  try {
+    res.status(200).json(req.user);
+  } catch (err) {
+    console.log("Error in CheckAuth Controller: ", err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
