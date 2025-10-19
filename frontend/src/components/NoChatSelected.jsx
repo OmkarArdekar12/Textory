4a64206b49440c615099d806430ad36c48ad610f
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-10-19 22:32:12 +0530

    Completed

---
+++ frontend/src/components/NoChatSelected.jsx
@@ -0,0 +1,51 @@
+const NoChatSelected = () => {
+  return (
+    <div className="w-full flex flex-1 flex-col items-center justify-center p-16 bg-base-100/50">
+      <div className="max-w-md text-center space-y-6">
+        <div className="flex justify-center gap-4 mb-4">
+          <div className="relative">
+            <div
+              className="relative w-16 h-16 rounded-2xl flex items-center justify-center animate-bounce"
+              style={{ animationTimingFunction: "ease" }}
+            >
+              <div className="absolute flex items-center gap-1">
+                <p
+                  className="rounded-full bg-white size-1 z-90 animate-bounce-small"
+                  style={{
+                    animationDelay: "0s",
+                    animationDuration: "2s",
+                    animationTimingFunction: "ease-in-out",
+                  }}
+                ></p>
+                <p
+                  className="rounded-full bg-white size-1 z-90 animate-bounce-medium"
+                  style={{
+                    animationDelay: "0.2s",
+                    animationDuration: "2s",
+                    animationTimingFunction: "ease-in-out",
+                  }}
+                ></p>
+                <p
+                  className="rounded-full bg-white size-1 z-90 animate-bounce-large"
+                  style={{
+                    animationDelay: "0.3s",
+                    animationDuration: "2s",
+                    animationTimingFunction: "ease-in-out",
+                  }}
+                ></p>
+              </div>
+              <img src="/Logo.png" alt="Textory" className="logo w-16 z-40" />
+            </div>
+          </div>
+        </div>
+
+        <h2 className="text-2xl font-bold">Welcome to Textory!</h2>
+        <p className="text-base-content/60">
+          Select a conversation from the sidebar to start chatting
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default NoChatSelected;
--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 
 export const useThemeStore = create((set) => ({
-  theme: localStorage.getItem("textory-theme") || "cupcake",
+  theme: localStorage.getItem("textory-theme") || "corporate",
   setTheme: (theme) => {
     localStorage.setItem("textory-theme", theme);
     set({ theme });
