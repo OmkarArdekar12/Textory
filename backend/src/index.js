0718dabc1cff5d4e587e52b9a6b8b02b9bed272f
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-10-20 17:17:53 +0530

    Completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from "url";
 
 import { connectDB } from "./lib/db.js";
 
@@ -14,11 +15,12 @@ import { app, server, io } from "./lib/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
-const __dirname = path.resolve();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://textory.onrender.com"],
     credentials: true,
   })
 );
